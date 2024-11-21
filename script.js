c08aa3b539f02d1a6f9e428002231f7dd416af4f
David Andersson <lejonmanen@gmail.com>
2024-11-21 09:49:08 +0100

    Basic HTML

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Hello world')
+
+// const TEST_KEY = 'test'
+// let data = localStorage.getItem(TEST_KEY)
+// console.log('Data från localStorage: ', data)
+
+// let word = {
+// 	word: 'gata',
+// 	translation: 'street'
+// }
+// let json = JSON.stringify( word )
+
+// localStorage.setItem(TEST_KEY, json)
+
